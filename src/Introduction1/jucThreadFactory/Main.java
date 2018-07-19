54f7ade20d41a6742540d4efd6bebecb82a20296
jianhang-lin <ej2741@email.vccs.edu>
2018-07-19 20:47:58 +0800

    利用ThreadFactory新启动线程

---
+++ src/Introduction1/jucThreadFactory/Main.java
@@ -0,0 +1,17 @@
+package Introduction1.jucThreadFactory;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+
+/**
+ * 利用ThreadFactory新启动线程
+ */
+public class Main {
+    public static void main(String[] args) {
+        ThreadFactory factory = Executors.defaultThreadFactory();
+        factory.newThread(new Printer("Nice!")).start();
+        for (int i = 0; i < 10000; i++) {
+            System.out.print("Good!");
+        }
+    }
+}

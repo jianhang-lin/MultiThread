b1da84c667b6efb5c1c46a5b0a1107f876a1b03f
jianhang-lin <ej2741@email.vccs.edu>
2018-07-19 17:55:45 +0800

    单线程程序

---
+++ src/Introduction1/SingleThread/Main.java
@@ -0,0 +1,12 @@
+package Introduction1.SingleThread;
+
+/**
+ * 单线程程序
+ */
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10000; i++) {
+            System.out.print("Good!");
+        }
+    }
+}

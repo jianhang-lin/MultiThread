f6a9772fbc6030a9899ef1d45906f75b0fc2991f
jianhang-lin <ej2741@email.vccs.edu>
2018-07-20 14:35:30 +0800

    Single Threaded Execution模式

---
+++ src/SingleThreadedExecution/Gate.java
@@ -0,0 +1,28 @@
+package SingleThreadedExecution;
+
+/**
+ * 表示门的类，它会在人们通过门时记录其姓名与出生地
+ */
+public class Gate {
+    private int counter = 0;
+    private String name = "Nobody";
+    private String address = "Nowhere";
+
+    public synchronized void pass(String name, String address) {
+        this.counter++;
+        this.name = name;
+        this.address = address;
+        check();
+    }
+
+    @Override
+    public synchronized String toString() {
+        return "No." + counter + ": " + name + ", " + address;
+    }
+
+    private void check() {
+        if (name.charAt(0) != address.charAt(0)) {
+            System.out.println("********** BROKEN ********* " + toString());
+        }
+    }
+}
---
+++ src/SingleThreadedExecution/UserThread.java
@@ -0,0 +1,24 @@
+package SingleThreadedExecution;
+
+/**
+ * 表示人的类.人们将不断地通过门
+ */
+public class UserThread extends Thread {
+    private final Gate gate;
+    private final String myname;
+    private final String myaddress;
+
+    public UserThread(Gate gate, String myname, String myaddress) {
+        this.gate = gate;
+        this.myname = myname;
+        this.myaddress = myaddress;
+    }
+
+    @Override
+    public void run() {
+        System.out.println(myname + " BEGIN");
+        while (true) {
+            gate.pass(myname, myaddress);
+        }
+    }
+}

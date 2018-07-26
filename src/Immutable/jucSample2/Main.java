050aa308e5fe5fcebcca792408c846d03a77e525
jianhang-lin <ej2741@email.vccs.edu>
2018-07-26 16:25:35 +0800

    利用Collections.synchronizedList方法所进行的同步

---
+++ src/Immutable/jucSample2/Main.java
@@ -0,0 +1,13 @@
+package Immutable.jucSample2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        final List<Integer> list = Collections.synchronizedList(new ArrayList<>());
+        new WriterThread(list).start();
+        new ReaderThread(list).start();
+    }
+}
---
+++ src/Immutable/jucSample2/ReaderThread.java
@@ -0,0 +1,24 @@
+package Immutable.jucSample2;
+
+import java.util.List;
+
+public class ReaderThread extends Thread {
+    private final List<Integer> list;
+
+    public ReaderThread(List<Integer> list) {
+        super("ReaderThread");
+        this.list = list;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            synchronized (list) {
+                for (int n :
+                        list) {
+                    System.out.println(n);
+                }
+            }
+        }
+    }
+}

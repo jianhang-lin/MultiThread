834de2a1a2ee6ae99af2de987b4a3ef644231f88
jianhang-lin <ej2741@email.vccs.edu>
2018-07-27 11:03:44 +0800

    示例程序

---
+++ src/GuardedSuspension/Sample/Request.java
@@ -0,0 +1,21 @@
+package GuardedSuspension.Sample;
+
+/**
+ * 表示一个请求的类
+ */
+public class Request {
+    private final String name;
+
+    public Request(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "[ Request " + name + " ]";
+    }
+}
---
+++ src/GuardedSuspension/Sample/RequestQueue.java
@@ -0,0 +1,27 @@
+package GuardedSuspension.Sample;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 依次存放请求的类
+ */
+public class RequestQueue {
+    private final Queue<Request> queue = new LinkedList<>();
+
+    public synchronized Request getRequest() {
+        while (queue.peek() == null) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+
+            }
+        }
+        return queue.remove();
+    }
+
+    public synchronized void putRequest(Request request) {
+        queue.offer(request);
+        notifyAll();
+    }
+}

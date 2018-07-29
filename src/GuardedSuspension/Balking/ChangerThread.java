731887417171b7401fd926d7a514a551c519be4d
jianhang-lin <ej2741@email.vccs.edu>
2018-07-29 11:03:11 +0800

    Bolking模式示例程序

---
+++ src/GuardedSuspension/Balking/ChangerThread.java
@@ -0,0 +1,32 @@
+package GuardedSuspension.Balking;
+
+import java.io.IOException;
+import java.util.Random;
+
+/**
+ * 修改并保存数据内容的类
+ */
+public class ChangerThread extends Thread {
+    private final Data data;
+    private final Random random = new Random();
+
+    public ChangerThread(String name, Data data) {
+        super(name);
+        this.data = data;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; true; i++) {
+                data.change("No." + i);//修改数据
+                Thread.sleep(random.nextInt(1000));//执行其他操作
+                data.save();//显式地保存
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
